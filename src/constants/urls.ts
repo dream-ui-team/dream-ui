b36a9099d4fe84d838c7c58e4c6e963b49722300
Gajanan Gaikwad <Gajanan.Gaikwad@nice.com>
2019-11-11 18:52:47 +0530

    First Commit

---
+++ src/constants/urls.ts
@@ -0,0 +1,4 @@
+export const urls = {
+    'Base': 'http://localhost:8080/'
+  };
+  
---
+++ src/redux/services/user.ts
@@ -0,0 +1,61 @@
+import { AsyncStorage } from "react-native";
+import { colors,urls } from "../../constants";
+import { Alert} from 'react-native';
+
+export function fetchImageService(page?: number, limit?: number) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://picsum.photos/v2/list?page=${page}&limit=${limit}`)
+      .then(res => {
+        return res.json();
+      })
+      .then(response => {
+        resolve(response);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
+export function loginUserService(username: string, password: string) {
+  return new Promise((resolve, reject) => {
+    fetch(`${urls.Base}users/login?userName=${encodeURIComponent(`${username}`)}&password=${encodeURIComponent(`${password}`)}`,{
+      method:'GET',
+      headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      }).then(res => res.json())
+        .then(response => {
+          resolve(response);
+        })    
+        .catch(error => {
+          Alert.alert("Login unSuccess");
+          reject(error);
+        });
+
+
+
+
+  //   let userToken = `${username}${password}`;
+  //   AsyncStorage.setItem("userToken", userToken)
+  //     .then(() => {
+  //       resolve(userToken);
+  //     })
+  //     .catch(error => {
+  //       reject(error);
+  //     });
+   });
+}
+
+export function logoutUserService() {
+  return new Promise((resolve, reject) => {
+    AsyncStorage.removeItem("userToken")
+      .then(() => {
+        resolve();
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
